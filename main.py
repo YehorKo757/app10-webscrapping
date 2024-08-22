0d057f591745a22a7dd1a0424dd5555672b6d961
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-22 22:23:29 +0100

    Add while loop, send email

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import selectorlib
-
+import os
+import smtplib, ssl
+import time
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
@@ -25,7 +27,18 @@ def store(extracted):
         file.write(extracted + "\n")
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "yehor.kosiachkin@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "yehor.kosiachkin@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
 
 
@@ -35,11 +48,17 @@ def read():
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read()
-    if extracted.lower() != "no upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        try:
+            scraped = scrape(URL)
+            extracted = extract(scraped)
+            print(extracted)
+            content = read()
+            if extracted.lower() != "no upcoming tours":
+                if extracted not in content:
+                    store(extracted)
+                    message = f"Hey, check out this new event {extracted}"
+                    send_email(message)
+            time.sleep(2)
+        except KeyboardInterrupt:
+            break
